44fadd41b7dcda6b742dc5ee402697f24f8f6d72
Shanzid1010 <hossainmdshanzid@gmail.com>
2026-02-09 15:26:21 +0600

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,5 @@
 
 import React from 'react';
-import { Hero } from './sections/Hero';
-import { About } from './sections/About';
-import { Services } from './sections/Services';
-import { Projects } from './sections/Projects';
-import { WhyChooseMe } from './sections/WhyChooseMe';
-import { Contact } from './sections/Contact';
-import { Footer } from './components/Footer';
 
 function App() {
   return (
