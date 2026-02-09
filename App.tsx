0e94f43b9bb28cb6706c0b5b770e21a80f3159fc
Shanzid1010 <hossainmdshanzid@gmail.com>
2026-02-09 15:22:51 +0600

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 
 import React from 'react';
-import { Navbar } from './components/Navbar';
 import { Hero } from './sections/Hero';
 import { About } from './sections/About';
 import { Services } from './sections/Services';
